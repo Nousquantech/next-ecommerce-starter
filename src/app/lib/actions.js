98f7e1b2cc9056d90d5e1a84dd14d2ef5778a3e0
Nousquantech <143766008+Nousquantech@users.noreply.github.com>
2024-05-27 10:32:54 -0400

    changes2

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -2,22 +2,22 @@ import { User, Product } from "./models";
 import { connectToDB } from "./utils";
 
 //fetch all users
-export const fetchUsers = async (q, page) => {
-  const regex = new RegExp(q, "i");
+// export const fetchUsers = async (q, page) => {
+//   const regex = new RegExp(q, "i");
 
-  const ITEMS_PER_PAGE = 5;
-  try {
-    connectToDB();
-    const count = await User.find({ username: { $regex: regex } }).count();
-    const users = await User.find({ username: { $regex: regex } })
-      .limit(ITEMS_PER_PAGE)
-      .skip(ITEMS_PER_PAGE * (page - 1));
-    return { count, users };
-  } catch (error) {
-    console.log(error);
-    throw new Error("Failed to fetch to users!");
-  }
-};
+//   const ITEMS_PER_PAGE = 5;
+//   try {
+//     connectToDB();
+//     const count = await User.find({ username: { $regex: regex } }).count();
+//     const users = await User.find({ username: { $regex: regex } })
+//       .limit(ITEMS_PER_PAGE)
+//       .skip(ITEMS_PER_PAGE * (page - 1));
+//     return { count, users };
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error("Failed to fetch to users!");
+//   }
+// };
 //fetch single users
 export const fetchUser = async (id) => {
   try {
